291700fb77e042375a2f3609bf6e08dfb9c920da
tsites32 <tsites32@gmail.com>
2015-12-08 09:45:20 -0500

    started programming the fingercharts

--- app/src/main/java/com/example/trevorsites/baritonefingerchart/FingerChart.java
+++ app/src/main/java/com/example/trevorsites/baritonefingerchart/FingerChart.java
@@ -7,15 +7,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by trevor.sites on 11/5/2015.
  */
-public class FingerChart extends ListActivity {
+public class FingerChart extends Activity {
+
+    Globals g;
+
+    int current_position;
+    int toggle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fingerchart);
+
+        g = (Globals) getApplication();
+
+        current_position = 0;
+        toggle = 0;
+
+        final ImageView valveone = (ImageView) findViewById(R. id. valveone);
+
+        final ImageView valvetwo = (ImageView) findViewById(R. id. valvetwo);
+
+        final ImageView valvethree = (ImageView) findViewById(R. id. valvethree);
+
+        final TextView notename = (TextView) findViewById(R. id. notename);
+
+        final ImageView noteimage = (ImageView) findViewById((R. id. noteimage));
+
+        final Spinner changenote = (Spinner) findViewById(R. id. changenote);
+
+        final String[] noteimages = g.getNoteImageBass();
+
     }
 }
