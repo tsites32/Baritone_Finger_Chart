3b7e4cefb62dc16e481605f06f5548b70fee7194
andrew.stimmell <andrewstimmell@gmail.com>
2015-12-09 09:28:41 -0500

    a;lskdjf;l'sakdjgf

--- app/src/main/java/com/example/trevorsites/baritonefingerchart/Globals.java
+++ app/src/main/java/com/example/trevorsites/baritonefingerchart/Globals.java
@@ -73,7 +73,7 @@ public class Globals extends Application {
     boolean [] alternatefingeringbollbass = {false, false, false, false, true, false, false, false,
         false, false, false, false, false, false, true, false, false, true, true, false, false,
         true, true, true, false, true, true, true, true, true, true, true, true, true, true,
-        true, true, true, true, true, true, true, true, };
+        true, true, true, true, true, true, true, true};
 
     boolean [][] alternatevalvesbass = {{false, false, false},
             {false, false, false},
@@ -143,5 +143,150 @@ public class Globals extends Application {
     public int getCurrentNotePosition() { return this.currentnoteposition; }
 
     public void setCurrentNotePosition (int i) {currentnoteposition = i; }
+
+
+
+
+
+
+
+
+
+    //Treble note code
+    String [] treblenotenames = {"Low F#", "Low Gb", "Low G", "Low G#", "Low Ab",
+            "Low A ", "Low A#", "Low Bb", "Low B", "Low C", "Low C#", "Low Db", "Low D",
+            "Low D#", "Low Eb", "Low E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb",
+            "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "High F", "High F#", "High Gb",
+            "High G", "High G#", "High Ab", "High A", "High A#", "High Bb", "High B", };
+
+    boolean[][] treblevalves = new boolean[][]{
+            {true, true, true},
+            {true, true, true},
+            {true, false, true},
+            {false, true, true},
+            {false, true, true},
+            {true, true, false},
+            {true, false, false},
+            {true, false, false},
+            {false, true, false},
+            {false, false, false},
+            {true, true, true},
+            {true, true, true},
+            {true, false, true},
+            {false, true, true},
+            {false, true, true},
+            {true, true, false},
+            {true, false, false},
+            {false, true, false},
+            {false, true, false},
+            {false, false, false},
+            {false, true, true},
+            {false, true, true},
+            {true, true, false},
+            {true, false, false},
+            {true, false, false},
+            {false, true, false},
+            {false, false, false},
+            {true, true, false},
+            {true, true, false},
+            {true, false, false},
+            {false, true, false},
+            {false, true, false},
+            {false, false, false},
+            {true, false, false},
+            {false, true, false},
+            {false, true, false},
+            {false, false, false},
+            {false, true, true},
+            {false, true, true},
+            {true, true, false},
+            {true, false, false},
+            {true, false, false},
+            {false, true, false}
+    };
+
+    String [] treblenoteimages = {"treble_low_f_sharp", "treble_low_g_flat", "treble_low_g",
+            "treble_low_g_sharp", "treble_low_a_flat", "treble_low_a", "treble_low_a_sharp",
+            "treble_low_b_flat", "treble_low_b", "treble_low_c", "treble_low_c_sharp",
+            "treble_low_d_flat", "treble_low_d", "treble_low_d_sharp", "treble_low_e_flat",
+            "treble_low_e", "treble_f", "treble_f_sharp", "treble_g_flat", "treble_g",
+            "treble_g_sharp", "treble_a_flat", "treble_a", "treble_a_sharp", "treble_b_flat",
+            "treble_b", "treble_c", "treble_c_sharp", "treble_d_flat", "treble_d",
+            "treble_d_sharp", "treble_e_flat", "treble_e", "treble_high_f",
+            "treble_high_f_sharp", "treble_high_g_flat", "treble_high_g", "treble_high_g_sharp",
+            "treble_high_a_flat", "treble_high_a", "treble_high_a_sharp", "treble_high_b_flat",
+            "treble_high_b", };
+
+    boolean [] alternatefingeringbolltreble = {false, false, false, false, false, true, false,
+            false, false, false, false, false, false, false, false, true, false, true, true,
+            true, false, false, true, true, true, true, true, true, true, true, true, true,
+            true, true, true, true, true, true, true, true, true, true, true};
+
+    boolean [][] alternatevalvestreble = {
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, true},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+            {false, false, true},
+            {false, false, false},
+            {true, true, true},
+            {true, true, true},
+            {true, false, true},
+            {false, false, false},
+            {false, false, false},
+            {false, false, true},
+            {true, true, true},
+            {true, true, true},
+            {true, false, true},
+            {false, true, true},
+            {false, false, true},
+            {false, false, true},
+            {true, false, true},
+            {false, true, true},
+            {false, true, true},
+            {false, false, true},
+            {true, false, true},
+            {true, true, true},
+            {true, true, true},
+            {true, false, true},
+            {true, true, true},
+            {true, true, true},
+            {true, false, true},
+            {false, true, true},
+            {false, true, true},
+            {true, true, false}
+    };
+
+    public String[] getTrebleNotesNames(){ return this.treblenotenames; }
+
+    public String getATrebleNoteName (int i){ return this.treblenotenames[i]; }
+
+    public boolean getValve1Treble (int i){ return this.treblevalves[i][0]; }
+
+    public boolean getValve2Treble (int i){ return this.treblevalves[i][1]; }
+
+    public boolean getValve3Treble (int i){ return this.treblevalves[i][2]; }
+
+    public String[] getNoteImageTreble() { return this.treblenoteimages; }
+
+    public boolean[] getAlternateFingeringTreble() { return this.alternatefingeringbolltreble; }
+
+    public boolean getAlternateValve1Treble (int i) { return this.alternatevalvestreble[i][0]; }
+
+    public boolean getAlternateValve2Treble (int i) { return this.alternatevalvestreble[i][1]; }
+
+    public boolean getAlternateValve3Treble (int i) { return this.alternatevalvestreble[i][2]; }
+
 
 }
